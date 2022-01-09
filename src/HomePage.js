d2ef0e7d81093d6f937177079ca7eed2e588a515
pt_3002 <prerna_tulsiani@hotmail.com>
2022-01-09 14:34:19 +0530

    navbar file created

---
+++ src/HomePage.js
@@ -0,0 +1,7 @@
+import React from 'react';
+
+const HomePage = () => {
+    return <h2>HOMEPAGE works</h2>
+};
+
+export default HomePage;
---
+++ src/NavBar/NavBar.js
@@ -0,0 +1,9 @@
+import React from 'react';
+
+import './NavBar.css';
+
+const NavBar = () => {
+    return <h2 className="navbar">NavBar works</h2>
+};
+
+export default NavBar;
