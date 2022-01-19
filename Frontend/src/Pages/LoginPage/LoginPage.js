735f11357387c599f2ef2dad849b1b6ee834db8d
Ritesh Narendra Chaudhari <76548605+Ritesh2408@users.noreply.github.com>
2022-01-19 16:20:45 +0530

    designed login/signup page

--- Frontend/src/Pages/LoginPage/LoginPage.js
+++ Frontend/src/Pages/LoginPage/LoginPage.js
@@ -1,42 +1,43 @@
-import React,{useState} from 'react';
+import React, { useState } from 'react';
 import axios from 'axios';
 import { useHistory } from "react-router-dom";
 
 import './LoginPage.css';
 
-const LoginPage = ({setLoginUser}) => {
+const LoginPage = ({ setLoginUser }) => {
 
     const history = useHistory()
 
-    const [user,setUser]=useState({
-        email:"",
-        password:""
+    const [user, setUser] = useState({
+        email: "",
+        password: ""
     })
 
     const handleChange = e => {
         const { name, value } = e.target
         setUser({
             ...user,
-            [name]:value
+            [name]: value
         })
     }
 
     const login_api = () => {
-        axios.post("http://localhost:9002/login",user)
-            .then(res => {alert(res.data.message)
-            setLoginUser(res.data.user)
-            history.push("/")
-        })
+        axios.post("http://localhost:9002/login", user)
+            .then(res => {
+                alert(res.data.message)
+                setLoginUser(res.data.user)
+                history.push("/")
+            })
     }
 
     return (
         <div className="login">
-            <h1>Login</h1>
-            <input type="text" name="email" value={user.email} placeholder='Enter your email' onChange={ handleChange }></input>
-            <input type="password" name="password" value={user.password} placeholder='Enter your password' onChange={ handleChange }></input>
-            <div className="button" onClick={login_api}>Login</div>
-            <div>or</div>
-            <div className="button" onClick={()=>history.push("/signup")}>Signup</div>
+            <h1 className="loginHeading">Login</h1>
+            <input className="inputClass" type="text" name="email" value={user.email} placeholder='Enter your email' onChange={handleChange}></input>
+            <input className="inputClass" type="password" name="password" value={user.password} placeholder='Enter your password' onChange={handleChange}></input>
+            <button className="button" onClick={login_api}>Login</button>
+            <div className='divOR'> or</div>
+            <button className="button" onClick={() => history.push("/signup")}>Signup</button>
         </div>
     );
 };
--- Frontend/src/Pages/Sign-UpPage/SignUpPage.js
+++ Frontend/src/Pages/Sign-UpPage/SignUpPage.js
@@ -8,44 +8,44 @@ const SignUpPage = () => {
 
     const history = useHistory()
 
-    const [user,setUser]=useState({
-        name:"",
-        email:"",
-        password:"",
-        reEnterpassword:""
+    const [user, setUser] = useState({
+        name: "",
+        email: "",
+        password: "",
+        reEnterpassword: ""
     })
 
     const handleChange = e => {
         const { name, value } = e.target
         setUser({
             ...user,
-            [name]:value
+            [name]: value
         })
     }
 
     const signup_api = () => {
-        const { name,email,password,reEnterpassword } = user
-        if( name && email && password && (password===reEnterpassword)){
-            axios.post("http://localhost:9002/signup",user)
-            .then( res=> alert(res.data.message))
+        const { name, email, password, reEnterpassword } = user
+        if (name && email && password && (password === reEnterpassword)) {
+            axios.post("http://localhost:9002/signup", user)
+                .then(res => alert(res.data.message))
             history.push("/login")
         }
-        else{
+        else {
             alert("Invalid input")
         }
-        
+
     }
 
     return (
         <div className="signup">
-            <h1>signup</h1>
-            <input type="text" name="name" value={user.name} placeholder='Your name' onChange={ handleChange }></input>
-            <input type="text" name="email" value={user.email} placeholder='Your email' onChange={ handleChange }></input>
-            <input type="password" name="password" value={user.password} placeholder='Your password' onChange={ handleChange }></input>
-            <input type="password" name="reEnterpassword" value={user.reEnterpassword} placeholder='Re-enter password' onChange={ handleChange }></input>
-            <div className="button" onClick={signup_api}>Signup</div>
-            <div>or</div>
-            <div className="button" onClick={()=>history.push("/login")}>Login</div>
+            <h1 className='signupHeading'>Sign Up</h1>
+            <input className='inputClass' type="text" name="name" value={user.name} placeholder='Your name' onChange={handleChange}></input>
+            <input className='inputClass' type="text" name="email" value={user.email} placeholder='Your email' onChange={handleChange}></input>
+            <input className='inputClass' type="password" name="password" value={user.password} placeholder='Your password' onChange={handleChange}></input>
+            <input className='inputClass' type="password" name="reEnterpassword" value={user.reEnterpassword} placeholder='Re-enter password' onChange={handleChange}></input>
+            <button className="button" onClick={signup_api}>Signup</button>
+            <div className='divOR'>or</div>
+            <button className="button" onClick={() => history.push("/login")}>Login</button>
         </div>
     );
 };
