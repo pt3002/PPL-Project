38fc74c8146c5ec36294ac51ed061bf6f404bfe8
pt_3002 <prerna_tulsiani@hotmail.com>
2022-01-25 19:31:51 +0530

    locationdetails.js pushed again

---
+++ Backend/Schema/LocationDetails.js
@@ -0,0 +1,37 @@
+const mongoose = require("mongoose");
+const { Schema } = mongoose;
+
+const LocationDetails = new Schema({
+    name: {
+        required: true,
+        type: String
+    },
+    title: {
+        required: true,
+        type: String
+    },
+    description: {
+        required: false,
+        type: String
+    },
+    besttimetotravel: {
+        required: true,
+        type: String
+    },
+    price: {
+        required: true,
+        type: Number
+    },
+    rating: {
+        required: true,
+        type: Number
+    },
+
+    image: {
+        required: true,
+        type: String
+    }
+
+});
+
+module.exports = mongoose.model("LocationDetails", LocationDetails);
