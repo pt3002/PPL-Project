f08b1b9a62a689c0209d92fb2f154c4e713b1147
pt_3002 <prerna_tulsiani@hotmail.com>
2022-01-25 16:19:39 +0530

    typing carousel for welcome page pushed

--- Frontend/src/Pages/WelcomePage/WelcomePage.js
+++ Frontend/src/Pages/WelcomePage/WelcomePage.js
@@ -1,6 +1,7 @@
 import React from 'react';
 import HomePage from '../HomePage/HomePage';
 import { useHistory } from "react-router-dom";
+import Typical from 'react-typical';
 
 import './WelcomePage.css';
 
@@ -8,7 +9,23 @@ const WelcomePage = () => {
     const history = useHistory()
 
     return(
-    <div>
+    <div className="welcomediv">
+        <h1 className="welcomeh1">Hi Welcome to the website</h1>
+        <p className="welcomep">
+            Want to{' '}
+            <Typical
+                loop={Infinity}
+                wrapper="b"
+                steps={[
+                    'Travel' ,
+                    100000,
+                    'Discover',
+                    100000,
+                    'Blog',
+                    100000
+                ]}
+            />
+        </p>
         <button className="button" onClick={() => history.push("/signup")}>Signup</button>
         <button className="button" onClick={() => history.push("/login")}>Login</button>
     </div>
