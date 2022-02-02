475fb0f1f5df0dfa49b564b17c2d9a7894cb2cd0
Ritesh Narendra Chaudhari <76548605+Ritesh2408@users.noreply.github.com>
2022-02-02 19:51:16 +0530

    Css done for login,signup and welcome

--- Frontend/src/Pages/Sign-UpPage/SignUpPage.js
+++ Frontend/src/Pages/Sign-UpPage/SignUpPage.js
@@ -5,7 +5,7 @@ import { useHistory } from "react-router-dom";
 import './SignUpPage.css';
 
 const SignUpPage = (props) => {
-    const {setLists}=props;
+    const { setLists } = props;
     const history = useHistory()
 
     const [user, setUser] = useState({
@@ -16,7 +16,7 @@ const SignUpPage = (props) => {
         stay: "",
         description: "",
         contactno: "",
-        interests:""
+        interests: ""
     })
 
     const handleChange = e => {
@@ -40,13 +40,13 @@ const SignUpPage = (props) => {
         }
 
     }
-    const userupdated=()=>{
+    const userupdated = () => {
         axios.get('http://localhost:9002/userslist')
-          .then(res=>setLists(res.data))
-          .catch(error => console.log(error));
-          console.log("userupdatedcalled");
-      }
-      
+            .then(res => setLists(res.data))
+            .catch(error => console.log(error));
+        console.log("userupdatedcalled");
+    }
+
 
 
 
@@ -54,14 +54,22 @@ const SignUpPage = (props) => {
     return (
         <div className="signup">
             <h1 className='signupHeading'>Sign Up</h1>
-            <input className='inputClass' type="text" name="name" value={user.name} placeholder='Your name' onChange={handleChange}></input>
-            <input className='inputClass' type="text" name="email" value={user.email} placeholder='Your email' onChange={handleChange}></input>
-            <input className='inputClass' type="password" name="password" value={user.password} placeholder='Your password' onChange={handleChange}></input>
-            <input className='inputClass' type="password" name="reEnterpassword" value={user.reEnterpassword} placeholder='Re-enter password' onChange={handleChange}></input>
-            <input className='inputClass' type="text" name="stay" value={user.stay} placeholder='Where are you from?' onChange={handleChange}></input>
-            <textarea className='inputClass' type="text" name="description" value={user.description} placeholder='Describe yourself' onChange={handleChange}></textarea>
-            <input className='inputClass' type="text" name="contactno" value={user.contactno} placeholder='Your contact no.' onChange={handleChange}></input>
-            <textarea className='inputClass' type="text" name="interests" value={user.interests} placeholder='Describe your interests' onChange={handleChange}></textarea>
+            <div className='club'>
+                <input className='inputClass' type="text" name="name" value={user.name} placeholder='Your name' onChange={handleChange}></input>
+                <input className='inputClass' type="text" name="email" value={user.email} placeholder='Your email' onChange={handleChange}></input>
+            </div>
+            <div className='club'>
+                <input className='inputClass' type="password" name="password" value={user.password} placeholder='Your password' onChange={handleChange}></input>
+                <input className='inputClass' type="password" name="reEnterpassword" value={user.reEnterpassword} placeholder='Re-enter password' onChange={handleChange}></input>
+            </div>
+            <div className='club'>
+                <input className='inputClass' type="text" name="contactno" value={user.contactno} placeholder='Your contact no.' onChange={handleChange}></input>
+                <input className='inputClass' type="text" name="stay" value={user.stay} placeholder='Where are you from?' onChange={handleChange}></input>
+            </div>
+            <div className='club'>
+                <textarea className='inputClass' type="text" name="description" value={user.description} placeholder='Describe yourself' onChange={handleChange}></textarea>
+                <textarea className='inputClass' type="text" name="interests" value={user.interests} placeholder='Describe your interests' onChange={handleChange}></textarea>
+            </div>
             <button className="button" onClick={signup_api}>Signup</button>
             <div className='divOR'>or</div>
             <button className="button" onClick={() => history.push("/login")}>Login</button>
--- Frontend/src/Pages/WelcomePage/WelcomePage.js
+++ Frontend/src/Pages/WelcomePage/WelcomePage.js
@@ -8,27 +8,33 @@ import './WelcomePage.css';
 const WelcomePage = () => {
     const history = useHistory()
 
-    return(
-    <div className="welcomediv">
-        <h1 className="welcomeh1">Hi Welcome to the website</h1>
-        <p className="welcomep">
-            Want to{' '}
-            <Typical
-                loop={Infinity}
-                wrapper="b"
-                steps={[
-                    'Travel' ,
-                    1000,
-                    'Discover',
-                    1000,
-                    'Blog',
-                    1000
-                ]}
-            />
-        </p>
-        <button className="button" onClick={() => history.push("/signup")}>Signup</button>
-        <button className="button" onClick={() => history.push("/login")}>Login</button>
-    </div>
+    return (
+        <div className="welcomediv">
+            <div className="welcomeh1">
+                <h1 className='welcomeheading'>Hi, Welcome to the website</h1>
+            </div>
+            <div className="welcomep">
+                <p>
+                    Want to{'   '}
+                    <Typical
+                        loop={Infinity}
+                        wrapper="b"
+                        steps={[
+                            'Travel',
+                            1000,
+                            'Discover',
+                            1000,
+                            'Blog',
+                            1000
+                        ]}
+                    />
+                </p>
+            </div>
+            <div className='welcomeButton'>
+                <button className="button" onClick={() => history.push("/signup")}>Signup</button>
+                <button className="button" onClick={() => history.push("/login")}>Login</button>
+            </div>
+        </div>
     );
 };
 
