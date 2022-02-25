af731a2b5eff9c157b072431e2eedc12e6bf6a7e
ShyamR12 <shyamrandar12@gmail.com>
2022-02-25 16:43:09 +0530

    aboutUs changes

--- Frontend/src/Pages/AboutUs/AboutUs.js
+++ Frontend/src/Pages/AboutUs/AboutUs.js
@@ -2,6 +2,7 @@ import React, { useState } from "react";
 
 import "./AboutUs.css";
 import HomePage from "../HomePage/HomePage";
+import AboutUsImage from "./AboutUs.jpg";
 
 const AboutUs = (props) => {
     return (
@@ -12,43 +13,32 @@ const AboutUs = (props) => {
                 <HomePage name={props.name}/>
             </div>
             <div>
-            <section className="about mx-auto" id="about">
-            <div className="container">
-                <div className="heading text-center">
-                    <h2>About
-                        <span>Us</span></h2>
-                    <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor
-                        <br/>
-                        incididunt ut labore et dolore magna aliqua.
-                    </p>
-                </div>
-                <div className="row">
-                    <div className="col-lg-6">
-                        <img src="https://i.ibb.co/qpz1hvM/About-us.jpg" alt="about" className="img-fluid" width="100%"/>
+                <section className="about mx-auto" id="about">
+                    <div className="container">
+                        <div className="heading text-center">
+                            <h2>AboutUs</h2>
+                            <p>We are dedicated to ensuring a superior user experience on our platform. <br />
+                                Plan your first wonderful trip by reading our suggestions under List of Places.
+                            </p>
+                        </div>
+                        <div className="row">
+                            <div className="col-lg-6">
+                                <img src={AboutUsImage} alt="about" className="img-fluid" width="100%" />
+                            </div>
+                            <div className="col-lg-6">
+                                <p>Have you ever said to yourself one of the following? <br />
+                                    "I want to travel more, but I don't know the first step."<br />
+                                    "How do I stay safe and healthy?"<br />
+                                    "How do I maximize my time?"<br />
+                                    You are not alone.<br />
+                                    <br />
+                                    Nurtured from the seed of a single great idea - to empower the traveller – Traveller's Arena is a pioneer in India's online travel guide/blog. Traveller's Arena came to life to empower the Indian traveller with instant travel suggestions by the travellers themselves. By using this website, you can spend less time searching the internet and being overwhelmed with information and more time doing the one thing you want to do: travel.
+
+                                </p>
+                            </div>
+                        </div>
                     </div>
-                    <div className="col-lg-6">
-                        <h3>Lorem ipsum dolor sit amet, consectetur </h3>
-                        <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod
-                            tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam,
-                            quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo
-                            consequat.</p>
-                    </div>
-                </div>
-                <div className="row mt-5">
-                    
-                    <div className="col-lg-6">
-                        <h3>Lorem ipsum dolor sit amet, consectetur </h3>
-                        <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod
-                            tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam,
-                            quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo
-                            consequat.</p>
-                    </div>
-                    <div className="col-lg-6">
-                        <img src="https://i.ibb.co/qpz1hvM/About-us.jpg" alt="about" className="img-fluid" width="100%"/>
-                    </div>
-                </div>
-            </div>
-        </section>
+                </section>
             </div>
 
         </div>
